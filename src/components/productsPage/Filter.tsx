496d82e3a1539f3cbb71bbf0e2eceb69ac43f52d
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-08-11 20:56:17 +0300

    filter.tsx simplification

--- src/components/productsPage/Filter.tsx
+++ src/components/productsPage/Filter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormContext, Controller, Control } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import { Product } from "@/types/productType";
 import FilterSection from "./FilterSection";
 import {
@@ -9,12 +9,14 @@ import {
 } from "@/styles/styled";
 import { priceRanges, sortOptions } from "@/lib/mockData";
 import { FilterState } from "@/types/filterType";
+
 interface FilterProps {
   data: Product[];
-  control: Control<FilterState, any>;
 }
+
 const Filter: React.FC<FilterProps> = ({ data }) => {
-  const { control, getValues, setValue } = useFormContext<FilterState>();
+  const { register, getValues, setValue, watch } =
+    useFormContext<FilterState>();
   const brands = data.map((item: Product) => item.brand);
   const uniqueBrands = Array.from(new Set(brands));
   const sellers = data.flatMap((item: Product) =>
@@ -25,6 +27,7 @@ const Filter: React.FC<FilterProps> = ({ data }) => {
       : []
   );
   const uniqueSellers = Array.from(new Set(sellers));
+
   const handlePriceRangeChange = (range: [number, number] | null) => {
     if (
       getValues("priceRange")?.[0] === range?.[0] &&
@@ -35,53 +38,49 @@ const Filter: React.FC<FilterProps> = ({ data }) => {
       setValue("priceRange", range);
     }
   };
+
+  const selectedBrands = watch("brands");
+  const selectedSortBy = watch("sortBy");
+  const selectedInStock = watch("inStock");
+  const selectedSellers = watch("sellers");
+
   return (
     <div>
       <FilterSection title="Markalar" isOpen={true}>
         {uniqueBrands.map((brand) => (
           <StyledLabel as="label" key={brand}>
-            <Controller
-              name="brands"
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="checkbox"
-                  checked={field.value.includes(brand)}
-                  onChange={() => {
-                    if (field.value.includes(brand)) {
-                      field.onChange(
-                        field.value.filter((b: string) => b !== brand)
-                      );
-                    } else {
-                      field.onChange([...field.value, brand]);
-                    }
-                  }}
-                />
-              )}
+            <input
+              type="checkbox"
+              value={brand}
+              {...register("brands")}
+              checked={selectedBrands.includes(brand)}
+              onChange={(e) => {
+                const valueArray = selectedBrands.includes(brand)
+                  ? selectedBrands.filter((b) => b !== brand)
+                  : [...selectedBrands, brand];
+                setValue("brands", valueArray);
+              }}
             />
             {brand}
           </StyledLabel>
         ))}
       </FilterSection>
+
       <FilterSection title="Sırala">
         {sortOptions.map(({ label, value }) => (
           <StyledRadioLabel as="label" key={value}>
-            <Controller
-              name="sortBy"
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="radio"
-                  name="sortBy"
-                  checked={field.value === value}
-                  onChange={() => field.onChange(value)}
-                />
-              )}
+            <input
+              type="radio"
+              value={value}
+              {...register("sortBy")}
+              checked={selectedSortBy === value}
+              onChange={() => setValue("sortBy", value)}
             />
             {label}
           </StyledRadioLabel>
         ))}
       </FilterSection>
+
       <FilterSection title="Peşin Fiyat" isOpen={true}>
         {priceRanges.map(({ label, range }) => (
           <StyledLabel as="label" key={label}>
@@ -97,42 +96,32 @@ const Filter: React.FC<FilterProps> = ({ data }) => {
           </StyledLabel>
         ))}
       </FilterSection>
+
       <StyledSwitchLabel>
-        <Controller
-          name="inStock"
-          control={control}
-          render={({ field }) => (
-            <input
-              type="checkbox"
-              checked={field.value}
-              onChange={() => field.onChange(!field.value)}
-            />
-          )}
+        <input
+          type="checkbox"
+          {...register("inStock")}
+          checked={selectedInStock}
+          onChange={() => setValue("inStock", !selectedInStock)}
         />
         Sadece stoktakiler
         <div className="switch"></div>
       </StyledSwitchLabel>
+
       <FilterSection title="Satıcılar" isOpen={true}>
         {uniqueSellers.map((seller) => (
           <StyledLabel as="label" key={seller}>
-            <Controller
-              name="sellers"
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="checkbox"
-                  checked={field.value.includes(seller)}
-                  onChange={() => {
-                    if (field.value.includes(seller)) {
-                      field.onChange(
-                        field.value.filter((s: string) => s !== seller)
-                      );
-                    } else {
-                      field.onChange([...field.value, seller]);
-                    }
-                  }}
-                />
-              )}
+            <input
+              type="checkbox"
+              value={seller}
+              {...register("sellers")}
+              checked={selectedSellers.includes(seller)}
+              onChange={(e) => {
+                const valueArray = selectedSellers.includes(seller)
+                  ? selectedSellers.filter((s) => s !== seller)
+                  : [...selectedSellers, seller];
+                setValue("sellers", valueArray);
+              }}
             />
             {seller}
           </StyledLabel>
